d5d263282c070040e27c9115d4db8b159d8e5ff5
freedom469 <bamjnr99@gmail.com>
2023-10-25 00:15:27 +0300

    myfamily

---
+++ myfamily.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -x
+# Check if HERO_ID is set in the environment
+if [ -z "$HERO_ID" ]; then
+  echo "HERO_ID environment variable is not set."
+  exit 1
+fi
+
+# Use curl to fetch the superhero data and jq to process it
+curl -s https://learn.zone01kisumu.ke/assets/superhero/all.json | jq -r --arg hero_id "$HERO_ID" '.[] | select(.id == ($hero_id | tonumber)) | .connections.relatives | gsub("\"";"")'
+
