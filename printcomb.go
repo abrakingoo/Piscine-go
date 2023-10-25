c15c5ba18274830f752172f949aa121e616bbdcc
abraham <maikoabraham68@gmail.com>
2023-10-25 18:51:40 +0300

    printcombo

--- printcomb.go
+++ printcomb.go
@@ -6,11 +6,16 @@ func PrintComb() {
 	for i := 0; i < 10; i++ {
 		for j := i + 1; j < 10; j++ {
 			for k := j + 1; k < 10; k++ {
-				z01.PrintRune(i, j, k)
+				z01.PrintRune(rune('0' + i))
+				z01.PrintRune(rune('0' + j))
+				z01.PrintRune(rune('0' + k))
+
 				if i < 7 {
-					z01.PrintRune(rune(', '))
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
 				} else if i == 7 && j < 8 {
-					z01.PrintRune(rune(', '))
+					z01.PrintRune(',')
+					z01.PrintRune(' ')
 				}
 			}
 		}
