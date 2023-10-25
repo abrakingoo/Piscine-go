a5c9ddec2977c4918f1d4d8e8433de2ef6ddc64f
abraham <maikoabraham68@gmail.com>
2023-10-25 17:08:01 +0300

    printreversealphabet

--- printreversealphabet/main.go
+++ printreversealphabet/main.go
@@ -3,8 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-    for char := 'z'; char >= 'a'; char-- {
-	z01.PrintRune(char)
-    }
-    z01.PrintRune('\n')
+	for char := 'z'; char >= 'a'; char-- {
+		z01.PrintRune(char)
+	}
+	z01.PrintRune('\n')
 }
