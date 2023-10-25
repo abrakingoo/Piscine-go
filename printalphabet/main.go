95625ba5081d50b90b9c5ca61392f1cce2cac134
abraham <maikoabraham68@gmail.com>
2023-10-25 15:36:49 +0300

    printalphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -2,14 +2,14 @@ package main
 
 import (
 
-    "fmt"
+    	"fmt"
 )
 
 func main() {
 
-    for ch := 'a'; ch <= 'z'; ch++ {
-        fmt.Printf("%c", ch)
-    }
+    	for ch := 'a'; ch <= 'z'; ch++ {
+        	fmt.Printf("%c", ch)
+    	}
 
-	fmt.Println('\n')
+		fmt.Println('\n')
 }
