95b6514607bb245dcd182e8d92b2906aa5831c60
abraham <maikoabraham68@gmail.com>
2023-10-25 16:35:57 +0300

    format

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,11 +3,8 @@ package main
 import "github.com/01-edu/z01"
 
 func main() {
-
-	// This is to print a to z using ascii code
 	for char := 'a'; char <= 'z'; char++ {
 		z01.PrintRune(char)
 	}
 	z01.PrintRune('\n')
-
 }
