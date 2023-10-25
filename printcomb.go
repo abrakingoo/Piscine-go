69a0c77d2e0bf47dcb36f099179381317f8b4a6f
abraham <maikoabraham68@gmail.com>
2023-10-25 18:53:48 +0300

    printcombo

--- printcomb.go
+++ printcomb.go
@@ -3,17 +3,14 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb() {
-	for i := 0; i < 10; i++ {
-		for j := i + 1; j < 10; j++ {
-			for k := j + 1; k < 10; k++ {
-				z01.PrintRune(rune('0' + i))
-				z01.PrintRune(rune('0' + j))
-				z01.PrintRune(rune('0' + k))
+	for i := '0'; i <= '7'; i++ {
+		for j := i + 1; j <= '8'; j++ {
+			for k := j + 1; k <= '9'; k++ {
+				z01.PrintRune(i)
+				z01.PrintRune(j)
+				z01.PrintRune(k)
 
-				if i < 7 {
-					z01.PrintRune(',')
-					z01.PrintRune(' ')
-				} else if i == 7 && j < 8 {
+				if i != '7' {
 					z01.PrintRune(',')
 					z01.PrintRune(' ')
 				}
