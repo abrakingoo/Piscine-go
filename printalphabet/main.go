c77f50f99b90166167ed4f5b4a09dccecc3dbc2c
abraham <maikoabraham68@gmail.com>
2023-10-25 15:58:51 +0300

    printalphabet

--- printalphabet/main.go
+++ printalphabet/main.go
@@ -3,15 +3,11 @@ package main
 import "fmt"
 
 func main() {
+
 	// This is to print a to z using ascii code
 	for char := 97; char <= 122; char++ {
 		fmt.Printf("%c", char)
 	}
 	fmt.Println("")
-
-	// Second example to print a to z using character
-	for char := 'a'; char <= 'z'; char++ {
-		fmt.Printf("%c", char)
-	}
 
 }
