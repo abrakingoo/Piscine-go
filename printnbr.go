ba9358eab8c7e153f9b849b35e9b53f8a2fa89b6
abraham <maikoabraham68@gmail.com>
2023-10-26 13:05:47 +0300

    printnbr.go

--- printnbr.go
+++ printnbr.go
@@ -3,6 +3,11 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintNbr(n int) {
+	if n == -9223372036854775808 {
+		z01.PrintRune('-')
+		z01.PrintRune('9')
+		n = 223372036854775808
+	}
 	if n < 0 {
 		z01.PrintRune('-')
 		n = -n
