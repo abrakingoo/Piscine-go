7f97db9b31b8f71b260b9f81610b4e6de118b6b7
abraham <maikoabraham68@gmail.com>
2023-10-27 08:47:49 +0300

    basicatoi

--- basicatoi.go
+++ basicatoi.go
@@ -3,7 +3,11 @@ package piscine
 func BasicAtoi(s string) int {
 	result := 0
 	for _, char := range s {
-		result = result*10 + int(char-'O')
+		digit := int(char - 'O')
+		if result > (1<<31-1-digit)/10 {
+			return 0
+		}
+		result = result*10 + digit
 	}
 	return result
 }
