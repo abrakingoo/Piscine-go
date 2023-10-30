5a90b6c697fa23517bff0586a6ae8b56f14df85a
Abu <maikoabraham68@gmail.com>
2023-10-30 12:15:03 +0300

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -1,8 +1,17 @@
 package piscine
 
+import "math"
+
 func IterativeFactorial(nb int) int {
-	result := 0
+	result := 1
+	
+	if nb < 0 {
+		return 0
+	}
 	for i := 1; i <= nb; i++ {
+		if result > math.MaxInt32/i {
+			return 0 
+		}
 		result *= i
 	}
 	return result
