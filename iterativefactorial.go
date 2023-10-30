386ba0ef837b176fcaf29c181d68a385eeb4d99d
Abu <maikoabraham68@gmail.com>
2023-10-30 12:17:18 +0300

    factorial

--- iterativefactorial.go
+++ iterativefactorial.go
@@ -4,13 +4,13 @@ import "math"
 
 func IterativeFactorial(nb int) int {
 	result := 1
-	
+
 	if nb < 0 {
 		return 0
 	}
 	for i := 1; i <= nb; i++ {
 		if result > math.MaxInt32/i {
-			return 0 
+			return 0
 		}
 		result *= i
 	}
