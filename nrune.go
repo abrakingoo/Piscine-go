6627cab7ab9e8d0f08a298ded66167aa50256612
Abraham <maikoabraham68@gmail.com>
2023-10-31 10:18:58 +0300

    nthrune

--- nrune.go
+++ nrune.go
@@ -3,7 +3,11 @@ package piscine
 func NRune(s string, n int) rune {
 	rRune := []rune(s)
 	if n >= 0 && n < len(rRune) {
-		return rRune[n-1]
+		for i := 0; i <= len(rRune); i++ {
+			if i == n {
+				return rRune[i]
+			}
+		}
 	} else if n < 0 && len(rRune)+n >= 0 {
 		return rRune[len(rRune)+n]
 	}
