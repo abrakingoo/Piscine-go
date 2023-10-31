7cf5f277beb41d88e6cdd4878af96ecd1009a40a
Abraham <maikoabraham68@gmail.com>
2023-10-31 12:14:22 +0300

    index

--- index.go
+++ index.go
@@ -2,8 +2,8 @@ package piscine
 
 func Index(s string, toFind string) int {
 	if len(toFind) == 0 {
-        return -1
-    }
+		return -1
+	}
 	find := toFind[0]
 	for i := 0; i < len(s); i++ {
 		if s[i] == find {
