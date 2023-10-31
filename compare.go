70911a516287b432a44662555d73f3889d41e373
Abraham <maikoabraham68@gmail.com>
2023-10-31 11:12:03 +0300

    compare

--- compare.go
+++ compare.go
@@ -1,10 +1,11 @@
 package piscine
 
 func Compare(a, b string) int {
-	if len(a) == len(b) {
+	if a == b {
 		return 0
-	} else if len(a) > len(b) {
-		return 1
 	}
-	return -1
+	if a < b {
+		return -1
+	}
+	return +1
 }
