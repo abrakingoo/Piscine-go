2d574c20372ef4a995d7e5ba0ac521f8c2a53b2b
Abraham <maikoabraham68@gmail.com>
2023-10-31 14:52:12 +0300

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -2,7 +2,7 @@ package piscine
 
 func IsAlpha(s string) bool {
 	for i := 0; i < len(s); i++ {
-		if !((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 0 && s[i] <= 9)) {
+		if !((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 48 && s[i] <= 57)) {
 			return false
 		}
 	}
