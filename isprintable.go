736da24bce94f880a08a89db074ab9fee030e91c
Abraham <maikoabraham68@gmail.com>
2023-10-31 15:10:53 +0300

    isprintable

--- isprintable.go
+++ isprintable.go
@@ -1,8 +1,8 @@
 package piscine
 
 func IsPrintable(s string) bool {
-	for i := 0; i < len(s); i++ {
-		if s[i] < 33 || s[i] > 126 {
+	for _, char := range s {
+		if char < 32 || char > 126 {
 			return false
 		}
 	}
