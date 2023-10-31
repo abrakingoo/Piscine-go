af4bedf5f2622d3c3f074345e88d42d3e67167ce
Abraham <maikoabraham68@gmail.com>
2023-10-31 17:47:42 +0300

    trimatoi

--- trimatoi.go
+++ trimatoi.go
@@ -6,13 +6,15 @@ func TrimAtoi(s string) int {
 	}
 	res := 0
 	sign := 1
+	prevChar := ' '
 	for _, char := range s {
-		if char == '-' {
+		if char == '-' && !(prevChar >= '0' && prevChar <= '9') {
 			sign = -1
 		}
 		if char >= '0' && char <= '9' {
 			res = res*10 + int(char-'0')
 		}
+		prevChar = char
 	}
 	return res * sign
 }
