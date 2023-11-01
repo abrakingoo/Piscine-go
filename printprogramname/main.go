4f60700c3e4ea4e1e6589855809d26a986df093f
Abraham <maikoabraham68@gmail.com>
2023-11-01 18:09:05 +0300

    printprogramme

---
+++ printprogramname/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/01-edu/z01"
+	"os"
+)
+
+func main() {
+	name := os.Args[0]
+	rname := []rune(name)
+
+	for i := 0; i < len(rname); i++ {
+		z01.PrintRune(rname[i])
+	}
+}
