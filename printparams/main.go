e77fdd2dfcdddcd64aa8cea358c666c31e7d06af
abraham <maikoabraham68@gmail.com>
2023-11-02 13:50:29 +0300

    params

---
+++ printparams/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+	for _, char := range args[1:] {
+		for i := 0; i < len(char); i++ {
+			z01.PrintRune(rune(char[i]))
+		}
+		z01.PrintRune('\n')
+	}
+	z01.PrintRune('\n')
+}
