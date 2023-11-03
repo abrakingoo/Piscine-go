449c0e4ddb4bba068a597afb95b557cb1992dc9f
Abu <maikoabraham68@gmail.com>
2023-11-03 11:47:25 +0300

    split

--- split.go
+++ split.go
@@ -5,7 +5,7 @@ func Split(s, sep string) []string {
 	sRune := []rune(s)
 	start := 0
 
-	for i, _ := range sRune {
+	for i := range sRune {
 		if string(sRune[i:i+len(sep)]) == sep {
 			new = append(new, string(sRune[start:i]))
 			start = i + len(sep)
