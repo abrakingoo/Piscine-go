7076e77d36325ee72f53a6911362a3cf8bc2f7c6
Abu <maikoabraham68@gmail.com>
2023-11-03 11:52:44 +0300

    split

--- split.go
+++ split.go
@@ -3,18 +3,18 @@ package piscine
 func Split(s, sep string) []string {
 	new := []string{}
 	sRune := []rune(s)
+	sepLen := len(sep)
 	start := 0
 
-	for i := range sRune {
-		if string(sRune[i:i+len(sep)]) == sep {
+	for i := 0; i < len(sRune); i++ {
+		if i+sepLen <= len(sRune) && string(sRune[i:i+sepLen]) == sep {
 			new = append(new, string(sRune[start:i]))
-			start = i + len(sep)
-			i += len(sep) - 1
+			start = i + sepLen
+			i += sepLen - 1
 		}
 	}
 	if start < len(sRune) {
 		new = append(new, string(sRune[start:]))
 	}
-
 	return new
 }
