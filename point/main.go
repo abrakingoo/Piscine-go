9e9b0617750354ff50080870682de4497806c829
Abraham <maikoabraham68@gmail.com>
2023-11-06 09:59:29 +0300

    pointer

---
+++ point/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
+}
+
+type point struct {
+	x int
+	y int
+}
+
+func main() {
+
+	points := &point{}
+
+	setPoint(*&points)
+
+	fmt.Printf("x = %d, y = %d\n", points.x, points.y)
+}
