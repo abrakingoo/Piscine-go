5c32b00fa49995260bbcd1223727e15a0f95c3f9
Abraham <maikoabraham68@gmail.com>
2023-11-06 12:38:37 +0300

    pointer

--- point/main.go
+++ point/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"github.com/01-edu/z01"
-)
+import "github.com/01-edu/z01"
 
 func setPoint(ptr *point) {
 	ptr.x = 42
@@ -14,26 +12,31 @@ type point struct {
 	y int
 }
 
-func printNumber(n int) []rune {
-	digits := []rune{}
-	for n > 0 {
-		d := n % 10
-		digits = append([]rune{rune('0' + d)}, digits...)
-		n /= 10
-	}
-
-	return digits
-}
-
 func main() {
 	points := &point{}
 
 	setPoint(points)
-	x := printNumber(points.x)
-	y := printNumber(points.y)
-	str := "x = " + string(x) + ", " + "y = " + string(y)
+	str := "x = " + convertToString(points.x) + ", y = " + convertToString(points.y)
 	for _, char := range str {
 		z01.PrintRune(char)
 	}
 	z01.PrintRune('\n')
+}
+
+func convertToString(n int) string {
+	if n == 0 {
+		return "0"
+	}
+	sign := ""
+	if n < 0 {
+		sign = "-"
+		n = -n
+	}
+	digits := ""
+	for n > 0 {
+		d := n % 10
+		digits = string('0'+d) + digits
+		n /= 10
+	}
+	return sign + digits
 }
