bbe1823ace4859c965b8b96abbf1fc69d991b7f3
Abraham <maikoabraham68@gmail.com>
2023-11-06 15:20:12 +0300

    point

--- point/main.go
+++ point/main.go
@@ -1,42 +1,20 @@
 package main
 
-import "github.com/01-edu/z01"
+import "fmt"
+
+type point struct {
+	x, y int
+}
 
 func setPoint(ptr *point) {
 	ptr.x = 42
 	ptr.y = 21
 }
 
-type point struct {
-	x int
-	y int
-}
-
 func main() {
-	points := &point{}
+	points:= new(point)
 
 	setPoint(points)
-	str := "x = " + convertToString(points.x) + ", y = " + convertToString(points.y)
-	for _, char := range str {
-		z01.PrintRune(char)
-	}
-	z01.PrintRune('\n')
-}
 
-func convertToString(n int) string {
-	if n == 0 {
-		return "0"
-	}
-	sign := ""
-	if n < 0 {
-		sign = "-"
-		n = -n
-	}
-	digits := ""
-	for n > 0 {
-		d := n % 10
-		digits = string('0'+d) + digits
-		n /= 10
-	}
-	return sign + digits
+	fmt.Printf("x = %d, y = %d\n",points.x, points.y)
 }
