67dfed6557981450f99b7157fa939bdf532ad376
Abraham <maikoabraham68@gmail.com>
2023-11-06 19:34:50 +0300

    point

--- point/main.go
+++ point/main.go
@@ -16,10 +16,15 @@ func setPoint(ptr *point) {
 
 func printValue(n int) {
 	divisor := 1
+	length := 0
+
 	for divisor <= n/10 {
 		divisor *= 10
+		length++
 	}
-	for divisor > 0 {
+	length++
+
+	for i := 0; i < length; i++ {
 		digit := n / divisor
 		z01.PrintRune('0' + digit)
 		n %= divisor
@@ -31,8 +36,12 @@ func main() {
 	points := &point{}
 	setPoint(points)
 	list := []int{points.y, points.x}
+	count := 0
+	for range list {
+		count++
+	}
 
-	for i := 0; i < len(list); i++ {
+	for i := 0; i < count; i++ {
 		printValue(list[i])
 		z01.PrintRune(' ')
 	}
