e2e397a3114dbdc95388b7e350bbac5f793b9dc8
Abraham <maikoabraham68@gmail.com>
2023-11-06 19:13:53 +0300

    point

--- point/main.go
+++ point/main.go
@@ -1,47 +1,43 @@
 package main
 
-import "github.com/01-edu/z01"
+import (
+	"github.com/01-edu/z01"
+)
 
 type point struct {
 	x int
 	y int
 }
 
-func main() {
-	points := &point{}
-	points.x = 42
-	points.y = 21
-
-	// Printing the values of x and y
-	printStr("x = ")
-	printInt(points.x)
-	printStr(", y = ")
-	printInt(points.y)
-	printStr("\n")
+func setPoint(ptr *point) {
+	ptr.x = 42
+	ptr.y = 21
 }
 
-func printStr(s string) {
-	for _, char := range s {
-		z01.PrintRune(char)
-	}
-}
-
-func printInt(n int) {
+func printNumber(n int) {
 	if n == 0 {
 		z01.PrintRune('0')
 		return
 	}
-	if n < 0 {
-		z01.PrintRune('-')
-		n = -n
+
+	digit := n % 10
+	rest := n / 10
+
+	if rest != 0 {
+		printNumber(rest)
 	}
-	var printHelper func(int)
-	printHelper = func(n int) {
-		if n == 0 {
-			return
-		}
-		printHelper(n / 10)
-		z01.PrintRune('0' + rune(n%10))
+	z01.PrintRune('0' + rune(digit))
+}
+
+func main() {
+	points := &point{}
+
+	setPoint(points)
+	list := []int{points.x, points.y}
+
+	for i := 0; i < len(list); i++ {
+		printNumber(list[i])
+		z01.PrintRune(' ')
 	}
-	printHelper(n)
+	z01.PrintRune('\n')
 }
