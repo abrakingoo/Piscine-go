6cf857c7e14ba5cb04f83a5fbae6d8a323e157d7
freedom469 <bamjnr99@gmail.com>
2023-11-10 01:02:44 +0300

    podiumposition

--- podiumposition.go
+++ podiumposition.go
@@ -1,10 +1,14 @@
 package piscine
 
 func PodiumPosition(podium [][]string) [][]string {
-	length := len(podium)
-	res := make([][]string, length)
-	for i := length - 1; i >= 0; i-- {
-		res[length-1-i] = podium[i]
+	res := podium
+
+	for j := 0; j < len(res); j++ {
+		for i := 0; i < len(res)-1; i++ {
+			if res[i][0] > res[i+1][0] {
+				res[i], res[i+1] = res[i+1], res[i]
+			}
+		}
 	}
 	return res
 }
