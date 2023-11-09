64a580d8a89d9d44dcd589ddd317e98d66b6b23a
Abraham <maikoabraham68@gmail.com>
2023-11-09 09:26:44 +0300

    combo

--- descendcomb.go
+++ descendcomb.go
@@ -2,15 +2,24 @@ package piscine
 
 import "github.com/01-edu/z01"
 
-func PrintComb() {
-	for i := '9'; i >= '0'; i++ {
-		for j := '9'; j >= '0'; j++ {
-			if i < j {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
+func DescendComb() {
+	for i := '9'; i >= '0'; i-- {
+		for j := '9'; j >= '0'; j-- {
+			for x := '9'; x >= '0'; x-- {
+				for y := '8'; y <= '0'; y-- {
+					if i < x || (i == x && j < y) {
+						z01.PrintRune(i)
+						z01.PrintRune(j)
+						z01.PrintRune(' ')
+						z01.PrintRune(x)
+						z01.PrintRune(y)
+						if !(i == '0' && j == '1' && x == '0' && y == '0') {
+							z01.PrintRune(',')
+							z01.PrintRune(' ')
+						}
+					}
+				}
 			}
-			z01.PrintRune(i)
-			z01.PrintRune(j)
 		}
 	}
 	z01.PrintRune('\n')
