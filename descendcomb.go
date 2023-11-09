a5a15fff88c7f54664aa29bad1ce7a235b43978a
Abraham <maikoabraham68@gmail.com>
2023-11-09 09:27:08 +0300

    combo

--- descendcomb.go
+++ descendcomb.go
@@ -6,7 +6,7 @@ func DescendComb() {
 	for i := '9'; i >= '0'; i-- {
 		for j := '9'; j >= '0'; j-- {
 			for x := '9'; x >= '0'; x-- {
-				for y := '8'; y <= '0'; y-- {
+				for y := '8'; y >= '0'; y-- {
 					if i < x || (i == x && j < y) {
 						z01.PrintRune(i)
 						z01.PrintRune(j)
