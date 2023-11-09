aa581157d11db6fc60ab40303caf9018855b413b
freedom469 <bamjnr99@gmail.com>
2023-11-09 21:13:00 +0300

    descend

--- descendappendrange.go
+++ descendappendrange.go
@@ -3,11 +3,10 @@ package piscine
 func DescendAppendRange(min, max int) []int {
 	res := []int{}
 
-	if max <= min {
-		return []int{}
-	}
-	for i := max; i > min; i-- {
-		res = append(res, i)
+	if max > min {
+		for i := max; i > min; i-- {
+			res = append(res, i)
+		}
 	}
 	return res
 }
