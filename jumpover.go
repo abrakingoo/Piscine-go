21497739025a90504bb1695cec5fe3ab44770d81
Abraham <maikoabraham68@gmail.com>
2023-11-09 16:43:06 +0300

    jumpover

---
+++ jumpover.go
@@ -0,0 +1,13 @@
+package piscine
+
+func JumpOver(str string) string {
+	if len(str) < 3 {
+		return "\n"
+	}
+	newS := ""
+
+	for i := 2; i < len(str); i += 3 {
+		newS += string(str[i])
+	}
+	return newS
+}
