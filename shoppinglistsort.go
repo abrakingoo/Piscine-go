a4ff5bb7067c1cc14b6eb88610aafd370ee5fe85
freedom469 <bamjnr99@gmail.com>
2023-11-09 21:45:22 +0300

    shoppinglistsort

--- shoppinglistsort.go
+++ shoppinglistsort.go
@@ -1,18 +1,18 @@
 package piscine
 
 func ShoppingListSort(slice []string) []string {
-	newList := []string{}
+	if len(slice) <= 1 {
+		return slice
+	}
 
 	for i := 0; i < len(slice); i++ {
 		for j := i + 1; j < len(slice); j++ {
 			if len(slice[i]) > len(slice[j]) {
-				slice[i], slice[j] = slice[j], slice[i]
+				temp := slice[i]
+				slice[i] = slice[j]
+				slice[j] = temp
 			}
 		}
 	}
-
-	for i := 0; i < len(slice); i++ {
-		newList = append(newList, slice[i])
-	}
-	return newList
+	return slice
 }
