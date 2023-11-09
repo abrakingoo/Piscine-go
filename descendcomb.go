5c37256158a9cd08e7bf7e53219edc9c8559dc99
freedom469 <bamjnr99@gmail.com>
2023-11-10 02:45:32 +0300

    discendcomb

--- descendcomb.go
+++ descendcomb.go
@@ -3,21 +3,19 @@ package piscine
 import "github.com/01-edu/z01"
 
 func DescendComb() {
-	for i := '9'; i >= '0'; i-- {
-		for j := '9'; j >= '0'; j-- {
-			for x := '9'; x >= '0'; x-- {
-				for y := '9'; y >= '0'; y-- {
-					if i > x || (i == x && j >= y) {
-						if !(i == '9' && j == '8' && x == '9' && y == '7') {
-							z01.PrintRune(i)
-							z01.PrintRune(j)
+	for i := 9; i >= 0; i-- {
+		for j := 9; j >= 0; j-- {
+			for k := 9; k >= 0; k-- {
+				for l := 9; l >= 0; l-- {
+					if (i != k || j != l) && (i*10+j) > (k*10+l) {
+						z01.PrintRune(rune(i + '0'))
+						z01.PrintRune(rune(j + '0'))
+						z01.PrintRune(' ')
+						z01.PrintRune(rune(k + '0'))
+						z01.PrintRune(rune(l + '0'))
+						if !(i == 0 && j == 1 && k == 0 && l == 0) {
+							z01.PrintRune(',')
 							z01.PrintRune(' ')
-							z01.PrintRune(x)
-							z01.PrintRune(y)
-							if !(i == '9' && j == '0' && x == '0' && y == '0') {
-								z01.PrintRune(',')
-								z01.PrintRune(' ')
-							}
 						}
 					}
 				}
