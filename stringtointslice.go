d41b0079c62c37968c34c3a6188fcb24d1d6ad42
Abraham <maikoabraham68@gmail.com>
2023-11-09 17:40:56 +0300

    string

--- stringtointslice.go
+++ stringtointslice.go
@@ -1,10 +1,13 @@
 package piscine
 
 func StringToIntSlice(str string) []int {
-	res := []int{}
 
-	for _, char := range str {
-		res = append(res, int(char))
-	}
+	if str != "" {
+		res := []int{}
+		for _, char := range str {
+			res = append(res, int(char))
+		}
 	return res
+	}
+	return []int(nil)
 }
