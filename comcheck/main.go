dd64e53799229c90ea89e3bb41a6c0954e721dd3
freedom469 <bamjnr99@gmail.com>
2023-11-09 22:45:26 +0300

    comcheck

--- comcheck/main.go
+++ comcheck/main.go
@@ -8,10 +8,13 @@ import (
 func main() {
 	keyWord := []string{"01", "galaxy", "galaxy 01"}
 	args := os.Args[1:]
+	alertPrinted := false
 	for _, char := range args {
 		for _, key := range keyWord {
-			if key == char {
+			if key == char && !alertPrinted {
 				fmt.Println("Alert!!!")
+				alertPrinted = true
+				break
 			}
 		}
 	}
