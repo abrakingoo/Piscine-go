5603739948946bfb58a153f7d28e52386b85ab9e
freedom469 <bamjnr99@gmail.com>
2023-11-09 19:53:11 +0300

    enigma

---
+++ enigma.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+	tempc := *******c
+	tempb := *b
+	tempd := ****d
+
+	*******c = ***a
+	****d = tempc
+	*b = tempd
+	***a = tempb
+}
