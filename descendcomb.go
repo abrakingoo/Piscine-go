fe91e660a02dae577a0f22467db04f508dcec1b3
Abraham <maikoabraham68@gmail.com>
2023-11-09 10:02:39 +0300

    descendcomb.go

--- descendcomb.go
+++ descendcomb.go
@@ -6,18 +6,23 @@ func DescendComb() {
 	for i := '9'; i >= '0'; i-- {
 		for j := '9'; j >= '0'; j-- {
 			for x := '9'; x >= '0'; x-- {
-				for y := '8'; y >= '0'; y-- {
-					z01.PrintRune(i)
-					z01.PrintRune(j)
-					z01.PrintRune(' ')
-					z01.PrintRune(x)
-					z01.PrintRune(y)
-					if !(i == '0' && j == '1' && x == '0' && y == '0') {
-						z01.PrintRune(',')
-						z01.PrintRune(' ')
+				for y := '9'; y >= '0'; y-- {
+					if i > x || (i == x && j >= y) {
+						if !(i == '9' && j == '8' && x == '9' && y == '7') {
+							z01.PrintRune(i)
+							z01.PrintRune(j)
+							z01.PrintRune(' ')
+							z01.PrintRune(x)
+							z01.PrintRune(y)
+							if !(i == '9' && j == '0' && x == '0' && y == '0') {
+								z01.PrintRune(',')
+								z01.PrintRune(' ')
+							}
+						}
 					}
 				}
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
