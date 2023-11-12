74d4f4a9eef5ce33d499a101280beb7911f44fc7
Abraham <maikoabraham68@gmail.com>
2023-11-12 11:38:09 +0300

    join

--- join.go
+++ join.go
@@ -2,7 +2,7 @@ package piscine
 
 func Join(strs []string, sep string) string {
 	var res string = strs[0]
-	for i := 0; i < len(strs[1:]); i++ {
+	for i := 1; i <= len(strs[1:]); i++ {
 		res = res + sep + string(strs[i])
 	}
 	return res
