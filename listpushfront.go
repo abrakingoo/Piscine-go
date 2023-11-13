34b106116254ed21e9c0489ccd0dd86c9418da7b
Abraham <maikoabraham68@gmail.com>
2023-11-13 18:59:33 +0300

    push

--- listpushfront.go
+++ listpushfront.go
@@ -1,18 +1,7 @@
 package piscine
 
-type NodeL struct {
-	Data interface{}
-	Next *NodeL
-}
-
-type List struct {
-	Head *NodeL
-	Tail *NodeL
-}
-
 func ListPushFront(l *List, data interface{}) {
 	new := &NodeL{Data: data}
-
 	if l.Head == nil {
 		l.Head = new
 		l.Tail = new
