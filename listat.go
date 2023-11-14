45608d4f4d485eec3d53509e645a5c493382e95c
Abraham <maikoabraham68@gmail.com>
2023-11-14 09:50:03 +0300

    listat

---
+++ listat.go
@@ -0,0 +1,16 @@
+package piscine
+
+func ListAt(l *NodeL, pos int) *NodeL {
+
+	current := l
+	count := 0
+
+	for current != nil && count <= pos {
+		if count == pos {
+			return current
+		}
+		current = current.Next
+	}
+
+	return nil
+}
