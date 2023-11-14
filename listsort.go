35683f83791f5db6feae6d3571f0e2a413da11c0
Abraham <maikoabraham68@gmail.com>
2023-11-14 14:09:16 +0300

    sort

---
+++ listsort.go
@@ -0,0 +1,24 @@
+package piscine
+
+type NodeI struct {
+	Data int
+	Next *NodeI
+}
+
+func ListSort(l *NodeI) *NodeI {
+	if l.Next == nil {
+		return l
+	}
+
+	current := l
+
+	for current != nil {
+		if current.Data > current.Next.Data {
+			temp := current.Data
+			current.Data = current.Next.Data
+			current.Next.Data = temp
+		}
+		current = current.Next
+	}
+	return l
+}
