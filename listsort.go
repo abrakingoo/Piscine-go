b29694b06ee7f96116871637ef5791562f43ec0d
Abraham <maikoabraham68@gmail.com>
2023-11-14 14:15:24 +0300

    sort

--- listsort.go
+++ listsort.go
@@ -1,23 +1,25 @@
 package piscine
 
-type NodeI struct {
-	Data int
-	Next *NodeI
-}
-
 func ListSort(l *NodeI) *NodeI {
 	if l == nil || l.Next == nil {
 		return l
 	}
-	current := l
 
-	for current != nil {
-		if current.Data > current.Next.Data {
-			temp := current.Data
-			current.Data = current.Next.Data
-			current.Next.Data = temp
+	isSorted := true
+	for isSorted {
+		isSorted = false
+		current := l
+
+		for current.Next != nil {
+			if current.Data > current.Next.Data {
+				temp := current.Data
+				current.Data = current.Next.Data
+				current.Next.Data = temp
+				isSorted = true
+			}
+			current = current.Next
 		}
-		current = current.Next
 	}
+
 	return l
 }
