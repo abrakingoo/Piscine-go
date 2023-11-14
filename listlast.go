bf6aeecb4bab44d803544b94eb88e36abbfe61e1
Abraham <maikoabraham68@gmail.com>
2023-11-14 09:03:14 +0300

    listlast

--- listlast.go
+++ listlast.go
@@ -1,8 +1,14 @@
 package piscine
 
 func ListLast(l *List) interface{} {
-	if l.Head != nil {
-		return l.Head.Data
+	if l.Head == nil {
+		return nil
 	}
-	return nil
+
+	current := l.Head
+	for current.Next != nil {
+		current = current.Next
+	}
+
+	return current.Data
 }
