ffc9e898eb75518982f5eeb2763bdd44bb91021c
abu <maikoabraham68@gmail.com>
2023-11-16 15:20:00 +0300

    isbinary

--- btreeisbinary.go
+++ btreeisbinary.go
@@ -4,8 +4,12 @@ func BTreeIsBinary(root *TreeNode) bool {
 	if root == nil {
 		return true
 	}
+
+	// If either left or right subtree is nil (not both)
 	if (root.Left == nil && root.Right != nil) || (root.Left != nil && root.Right == nil) {
 		return false
 	}
+
+	// Recursively check both left and right subtrees
 	return BTreeIsBinary(root.Left) && BTreeIsBinary(root.Right)
 }
