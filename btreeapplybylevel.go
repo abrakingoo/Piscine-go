db87a13f894d6079860e5116e839585a4ddfe117
abu <maikoabraham68@gmail.com>
2023-11-17 09:41:50 +0300

    apply by level

---
+++ btreeapplybylevel.go
@@ -0,0 +1,22 @@
+package piscine
+
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+
+	if root == nil {
+		return
+	}
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		base := queue[0]
+		queue = queue[1:]
+
+		f(base.Data)
+
+		if base.Left != nil {
+			queue = append(queue, base.Left)
+		}
+		if base.Right != nil {
+			queue = append(queue, base.Right)
+		}
+	}
+}
